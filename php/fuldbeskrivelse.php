589f34f8a964a5188cf8ed931e3018dd3fdb3c43
Mathias Hasseltoft Duus Nielsen <math864n@edu.sde.dk>
2020-06-30 10:08:45 +0200

    V. 0.5.6
    misc
    - removed Bootstrap folder

--- php/fuldbeskrivelse.php
+++ php/fuldbeskrivelse.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link href="../css/Style.css" rel="stylesheet" type="text/css"/>
-        <link href="../Bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/bootstrap.css" rel="stylesheet" type="text/css"/>
         
         <title>Opgaver </title>
     </head>
